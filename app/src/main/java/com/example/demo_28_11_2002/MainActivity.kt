3f19aa7180f5663582960247735359c3a3df9657
hp <huydqph14281@fpt.edu.vn>
2022-11-29 13:53:02 +0700

    2 comit

--- app/src/main/java/com/example/demo_28_11_2002/MainActivity.kt
+++ app/src/main/java/com/example/demo_28_11_2002/MainActivity.kt
@@ -11,7 +11,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
         //hello.
     }
 }
