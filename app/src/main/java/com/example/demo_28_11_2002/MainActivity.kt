80f1198e49418dc748aeebef3aff50b7894034d5
hp <huydqph14281@fpt.edu.vn>
2022-11-29 11:01:11 +0700

    Hi this ís first commit

---
+++ app/src/main/java/com/example/demo_28_11_2002/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.demo_28_11_2002
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+import android.widget.TextView
+import com.google.android.material.button.MaterialButton
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+    }
+}
